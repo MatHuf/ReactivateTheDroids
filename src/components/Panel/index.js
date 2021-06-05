472c68c7fc31864ab0cbcd76d34a310be15d074e
MatHuf <53545249+MatHuf@users.noreply.github.com>
2021-06-05 16:12:27 -0400

    Setting up footer for animated display

--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -13,10 +13,32 @@ const Panel = props => {
 	return (
 		<div className="panel-container">
 			<div className="data-section"></div>
-			<div className="droid-section"></div>
+			<div className="droid-section">
+				<div className="droid-footer">
+					<div className="footer-outer">
+						<div className="footer-inner footer-inner-left">
+							<div className="faux-button faux-button-left"></div>
+						</div>
+					</div>
+					<div className="footer-center-outer">
+						<div className="faux-button faux-button-triple"></div>
+						<div className="faux-button faux-button-text"></div>
+						<div className="faux-button faux-button-center"></div>
+						<div className="faux-button faux-button-text"></div>
+						<div className="faux-button faux-button-triple"></div>
+					</div>
+					<div className="footer-outer">
+						<div className="footer-inner footer-inner-right">
+							<div className="faux-button faux-button-right"></div>
+						</div>
+					</div>
+				</div>
+			</div>
 			<div className="controls-section">
+				<div className="controls-deco circle"></div>
 				<ActionButton action={deactivate} />
 				<ActionButton action={activate} />
+				<div className="controls-deco triangle"></div>
 			</div>
 		</div>
 	);
